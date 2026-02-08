38a4ced78fa70bc5154da3584ecbe30d950d1efb
Joshua Ocansey <joshocan@gmail.com>
2026-02-08 16:28:11 +0100

    add well-formedness checker and evaluation paths

--- fame/evaluation/__init__.py
+++ fame/evaluation/__init__.py
@@ -7,5 +7,12 @@ Designed to be extended (structure, constraints, traceability, etc.).
 """
 
 from .coverage import CoverageEvaluator, CoverageConfig, coverage_score
+from .wellformed import validate_feature_model, WellFormedResult
 
-__all__ = ["CoverageEvaluator", "CoverageConfig", "coverage_score"]
+__all__ = [
+    "CoverageEvaluator",
+    "CoverageConfig",
+    "coverage_score",
+    "validate_feature_model",
+    "WellFormedResult",
+]
